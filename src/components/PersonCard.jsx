30cb7c3492f33b8e626ab93cd11bd69aec9679e7
Jenn Nguyen <jennguy17@gmail.com>
2021-10-27 20:43:04 -0700

    first commit

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           age: this.props.age
+        };
+    }
+
+    addOne = () => {
+        this.setState({ age: this.state.age + 1 });
+    };
+
+    render() {
+        const { firstName, lastName, hairColor } = this.props;
+        return (
+            <div>
+                <h1>{lastName} {firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick = { this.addOne }>Birthday Button for {firstName} {lastName}</button>
+            </div>
+        )
+    }
+
+}
+
+export default PersonCard;
